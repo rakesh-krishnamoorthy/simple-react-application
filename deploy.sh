7e4f2783e13c7d4d08d05dede06fff88f2f1b1c5
RAKESH K <87237395+rakesh-krishnamoorthy@users.noreply.github.com>
2024-06-13 17:03:41 +0530

    modified deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,11 +3,14 @@ set -e
 
 #install nodejs
 sudo apt update
-sudo apt install nodejs npm
+sudo apt install -y nodejs npm
 
 #Checking the installation
 npm -v
 node -v
+
+#installing npm
+npm install
 
 #starting the application
 npm start
