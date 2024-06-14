022321dcf068d454adfb8deb8ac54b07ba3ef42b
RAKESH K <87237395+rakesh-krishnamoorthy@users.noreply.github.com>
2024-06-14 11:25:49 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -12,5 +12,12 @@ node -v
 #installing npm
 npm install
 
-#starting the application
-npm start
+
+# Killing the existing process
+pm2 delete react-app || true
+
+# Start the application using pm2
+pm2 start /home/ubuntu/simple-react-application/src/index.js --name react-app
+ 
+# Saving pm2 process list 
+pm2 save
